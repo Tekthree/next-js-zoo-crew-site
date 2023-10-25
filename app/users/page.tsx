ee8009e1e9a905c13d0ee4e67210bfd95756acec
Tek Jones <tekthree@gmail.com>
2023-10-25 10:04:33 -0700

    adds api caall example

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { link } from 'fs';
+import React from 'react';
+
+interface User {
+  id: number;
+  name: string;
+}
 
 const UsersPage = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: User[] = await res.json();
+
   return (
-    <div>UsersPage</div>
-  )
-}
+    <>
+      <h1 className='mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl'>
+        Loser Users
+      </h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name} </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default UsersPage;
